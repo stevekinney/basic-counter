3c5ad59f4c6e2052b12c7141a81e230710a76f55
Steve Kinney <hello@stevekinney.net>
2017-11-16 11:04:42 -0600

    Implement basic count

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 
 export default class Counter extends Component {
+  state = {
+    count: 0
+  }
+
+  handleIncrement = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  handleDecrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  }
+
+  reset = () => {
+    this.setState({
+      count: 0
+    });
+  }
+
   render() {
+    const { count } = this.state;
     return (
       <section className="Counter">
-        <h1>Count: {0}</h1>
-        <button onClick={() => {}} className="full-width">Increment</button>
-        <button onClick={() => {}} className="full-width">Decrement</button>
-        <button onClick={() => {}} className="full-width">Reset</button>
+        <h1>Count: {count}</h1>
+        <button onClick={this.handleIncrement} className="full-width">Increment</button>
+        <button onClick={this.handleDecrement} className="full-width">Decrement</button>
+        <button onClick={this.reset} className="full-width">Reset</button>
       </section>
     );
   }
